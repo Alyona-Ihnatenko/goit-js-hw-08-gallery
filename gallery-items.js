1a75de71d56ee6b7bab3492149bb733076c9f630
Alyona Ihnatenko <altaig4@gmail.com>
2021-05-24 23:02:53 +0300

    create-08

--- gallery-items.js
+++ gallery-items.js
@@ -65,27 +65,76 @@ const images= [
     },
   ];
 
-  const gallery=document.querySelector('.js-gallery');
-  gallery.insertAdjacentHTML('afterbegin',createGalleryEl(images));
+  const refs = {
+  galleryList: document.querySelector('ul.gallery'),
+  lightbox: document.querySelector('.lightbox'),
+  btn: document.querySelector('[data-action="close-lightbox"]')
+};
 
-  function createGalleryEl(images){
-      return images.map(({preview, original, description})=>{
-          return `<li class="gallery__item"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></li>`;
-      }).join("");
-  };
-  const image=document.querySelectorAll('.gallery__image')
-  const lightboxImage=document.querySelector('.lightbox__image');
-  const lightbox=document.querySelector('.lightbox');
-  const arrayImage=[];
 
-  gallery.addEventListener('click',onClickImage);
+   
 
-  function onClickImage(event){
-    if(!event.target.classList.contains('.gallery__image')){
-      return;
-    }else{
-      gallery.classList.add('.is-open');
-    }
-    
+const createImage = (item, parent) => {
+  const { preview, original, description } = item;
+  const img = document.createElement('img');
+  
+  img.classList.add('gallery__image');
+  img.dataset.source = original;
+  img.src = preview;
+  img.alt = description;
+  
+  parent.appendChild(img);
+};
+
+const createLink = (item, parent) => {
+  const { original } = item;
+  const a = document.createElement('a');
+  
+  a.classList.add('gallery__link');
+  a.href = original;
+  
+  createImage(item, a);
+  
+  parent.appendChild(a);
+};
+
+const createItem = (item) => {
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+  
+  createLink(item, li);
+  
+  return li;
+};
+
+const renderListItems = (arr) => {
+  const items = arr.map( (item) => createItem(item));
+  
+  refs.galleryList.append(...items);
+};
+
+renderListItems(images);
+
+
+
+
+function onClickHandler(e) {
+  e.preventDefault();
+  
+  if(e.target.nodeName === 'IMG') {
+    refs.lightbox.classList.add('is-open');
+    refs.lightbox.querySelector('.lightbox__image').src = e.target.src;
+    refs.lightbox.querySelector('.lightbox__image').alt = e.target.alt;
   }
-  console.log(onClickImage)
+}
+
+function onCloseHandler(e) {
+  if(e.target.nodeName === "I" || e.target.nodeName === "BUTTON") {
+    refs.lightbox.classList.remove('is-open');
+  }
+}
+
+refs.galleryList.addEventListener('click', onClickHandler);
+refs.btn.addEventListener('click', onCloseHandler);
+  
+   
